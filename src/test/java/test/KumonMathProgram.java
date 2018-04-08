76d9dc44871b9927ca597e2e40e4761670310250
pKPallavi <>
2018-04-08 00:37:43 -0500

    Updated KumonReadingProgram.java

--- src/test/java/test/KumonMathProgram.java
+++ src/test/java/test/KumonMathProgram.java
@@ -103,7 +103,7 @@ public class KumonMathProgram {
             Thread.sleep(1000);
         }
 
-        driver.findElement(By.linkText("Ages 3-5")).click();
+        /*driver.findElement(By.linkText("Ages 3-5")).click();
         try {
             assertEquals(driver.findElement(By.xpath("//div[@id='item-1_3-6']/h5")).getText(), "Build your child’s math skills from an early age");
         } catch (Error e) {
@@ -131,7 +131,7 @@ public class KumonMathProgram {
             } catch (Exception e) {
             }
             Thread.sleep(1000);
-        }
+        }*/
 
         driver.findElement(By.linkText("Ages 6-12")).click();
         try {
